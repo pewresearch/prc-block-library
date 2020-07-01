b112b6b6792f8f76639cb82421ec3878a090ab35
Seth Rubenstein <seth@sethrubenstein.info>
2020-07-01 15:29:21 -0400

    Mods to patterns

--- patterns/one-lede.php
+++ patterns/one-lede.php
@@ -20,11 +20,6 @@ ob_start();
 <hr class="wp-block-separator"/>
 <!-- /wp:separator -->
 
-<!-- wp:prc-block/promo {"header":"Facts are more important than ever.","description":"\u003cp\u003eIn times of uncertainty, good decisions demand good data. Please support our research with a financial contribution.\u003c/p\u003e","backgroundColor":"#F7F7F2","borderColor":"#D8D8D8","className":"pancake"} -->
-<div class="wp-block-prc-block-promo pancake" style="border-color:#D8D8D8"><div class="text"><h2>Facts are more important than ever.</h2><div><p>In times of uncertainty, good decisions demand good data. Please support our research with a financial contribution.</p></div></div><div class="action"><!-- wp:prc-block/button {"color":"#d3aa20","label":"DONATE"} -->
-<a class="wp-block-prc-block-button mustard ui button" href="">DONATE</a>
-<!-- /wp:prc-block/button --></div></div>
-<!-- /wp:prc-block/promo --></div></div>
 <!-- /wp:group -->
 <?php
 $content = ob_get_clean();
