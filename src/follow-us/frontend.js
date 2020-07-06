00089f770f935bb10b0827fad05af7c8c304f8dc
Seth Rubenstein <seth@sethrubenstein.info>
2020-07-06 12:04:32 -0400

    Update amp and ensure api-fetch for accounts system

--- src/follow-us/frontend.js
+++ src/follow-us/frontend.js
@@ -1,21 +1,18 @@
 import { render } from '@wordpress/element';
 
+import domReady from '@wordpress/dom-ready';
 import FollowUs from './component';
 
-// When DOM is fully loaded load the filters
-document.addEventListener("DOMContentLoaded", function(){
-	if ( document.querySelector('.js-react-follow-us') ) {
-		const elms = document.querySelectorAll('.js-react-follow-us');
-		for ( let elm of elms ) {
-			const socialMediaMarkup = elm.innerHTML;
-			const props = {
-				newsletters: elm.getAttribute('data-newsletters'),
-				setAttributes: false,
-			}
-			render( 
-				<FollowUs {...props}>{socialMediaMarkup}</FollowUs>,
-				elm
-			);	
-		}
-	}
+domReady(() => {
+    if (document.querySelector('.js-react-follow-us')) {
+        const elms = document.querySelectorAll('.js-react-follow-us');
+        elms.forEach(elm => {
+            const socialMediaMarkup = elm.innerHTML;
+            const props = {
+                newsletters: elm.getAttribute('data-newsletters'),
+                setAttributes: false,
+            };
+            render(<FollowUs {...props}>{socialMediaMarkup}</FollowUs>, elm);
+        });
+    }
 });
