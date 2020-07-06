da28533721b25bfe0958ff582b639c903c578750
Chris Baronavski <chris@punc.tilio.us>
2020-07-06 17:25:56 -0400

    WIP mailchimp opt-down form working

--- src/mailchimp-opt-down/component.js
+++ src/mailchimp-opt-down/component.js
@@ -1,6 +1,6 @@
 import { Component, Fragment, RawHTML } from '@wordpress/element';
 import { withState } from '@wordpress/compose';
-import { Form, Input, Dimmer, Button } from 'semantic-ui-react';
+import { Form, Input, Dimmer, Button, Message } from 'semantic-ui-react';
 
 const defaultState = {
     error: false,
@@ -8,7 +8,7 @@ const defaultState = {
     list_id: 'a33430a835', // Mailchimp ID for Quarterly Update newsletter
     choice: true,
     dimmerActive: false,
-    dimmerMessage: 'BLANK DIMMER MESSAGE',
+    dimmerMessage: '',
 };
 
 const MailchimpOptDown = withState(defaultState)(({
@@ -23,13 +23,26 @@ const MailchimpOptDown = withState(defaultState)(({
         setState,
     }) => {
 
+        const validate_email = e => {
+          return /^.+@.+\..+$/.test(e);
+        };
+
         const submitHandler = e => {
           e.preventDefault();
           const email = emailAddress;
-          let updated_interests = [];
+          if (!validate_email(email)) {
+            console.log('email error');
+            setState({
+              dimmerMessage: 'Please enter a valid email address.' ,
+              error: true
+            });
+
+            return;
+          }
+          let updated_interests = {};
 
           jQuery.each(jQuery("input[name='interests']"), function(){
-              updated_interests.push({[jQuery(this).val()] : jQuery(this).is(":checked") ? true : false });
+              jQuery.extend(updated_interests, {[jQuery(this).val()] : jQuery(this).is(":checked") ? true : false });
           });
 
           setTimeout(() => {
@@ -44,11 +57,22 @@ const MailchimpOptDown = withState(defaultState)(({
                           })}`,
                       type: 'POST',
                   })
+                  .fail( e => {
+                    console.log(e);
+                    if (e.responseJSON.data.status == '404') {
+                      setState({
+                          dimmerMessage:
+                              'We could not find that email address in our records.',
+                          error: true
+                      });
+                    }
+                  })
                   .done(() => {
                       console.info('Succesfully subscribed');
                       setState({
                           dimmerMessage:
-                              'You have succesfully subscribed to this newsletter.',
+                              'You have succesfully updated your preferences.',
+                          error: false
                       });
                   })
           });
@@ -60,30 +84,38 @@ const MailchimpOptDown = withState(defaultState)(({
         console.log(emailAddress);
 
         return (
-          <Form className="mailchimp-opt-down">
-            <Form.Input
-              label="Email"
-              placeholder="Your email address"
-              name="email"
-              data-validate="mc-email"
-              required
-              value={emailAddress}
-              onChange={ e => { setState({emailAddress: e.target.value}) } }
-              />
-              { interests.map( (d,i) =>
-                <Form.Checkbox
-                  inline
-                  name="interests"
-                  label={d.value === list_id ? `Send me quarterly updates and unsubscribe from all other newsletters.` : d.label }
-                  value={d.value}
-                  readonly={d.value !== list_id ? true : false }
-                  defaultChecked={d.value === list_id ? true : false }
-                  style={{ 'display' : d.value !== list_id ? `none` : `inherit` }}
-                  onChange={ e => { setState({choice: !choice }) } }
+          <>
+            <Message
+              style={{fontFamily: 'franklin-gothic-urw'}}
+              color={ error ? 'red' : 'green'}
+              hidden={dimmerMessage.length > 0 ? false : true }>
+              {dimmerMessage}
+            </Message>
+            <Form className="mailchimp-opt-down">
+              <Form.Input
+                label="Email"
+                placeholder="Your email address"
+                name="email"
+                data-validate="mc-email"
+                required
+                value={emailAddress}
+                onChange={ e => { setState({emailAddress: e.target.value}) } }
                 />
-              )}
-            <Button disabled={ !choice || emailAddress.length == 0 } onClick={submitHandler}>Update preferences</Button>
-          </Form>
+                { interests.map( (d,i) =>
+                  <Form.Checkbox
+                    inline
+                    name="interests"
+                    label={d.value === list_id ? `Send me quarterly updates and unsubscribe from all other newsletters.` : d.label }
+                    value={d.value}
+                    readonly={d.value !== list_id ? true : false }
+                    defaultChecked={d.value === list_id ? true : false }
+                    style={{ 'display' : d.value !== list_id ? `none` : `inherit` }}
+                    onChange={ e => { setState({choice: !choice }) } }
+                  />
+                )}
+              <Button disabled={ !choice } onClick={submitHandler}>Update preferences</Button>
+            </Form>
+          </>
         );
     },
 );
