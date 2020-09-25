d8f2aba1ec1d9bbff5f5906c9b54255eb2f71fdc
Seth Rubenstein <seth@sethrubenstein.info>
2020-09-25 12:53:11 -0400

    Finished changes to flip cards, now finishing cover mobile

--- src/flip-card/edit.js
+++ src/flip-card/edit.js
@@ -1,5 +1,13 @@
-import { InnerBlocks } from '@wordpress/block-editor';
-import { ResizableBox } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { InnerBlocks, InspectorControls } from '@wordpress/block-editor';
+import {
+    ColorPalette,
+    PanelBody,
+    PanelRow,
+    TextControl,
+    ResizableBox,
+} from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
 import { dispatch } from '@wordpress/data';
 import './edit.scss';
 
@@ -8,45 +16,110 @@ const TEMPLATE = [
     ['prc-block/flip-card-front', {}],
     ['prc-block/flip-card-back', {}],
 ];
+const bgDefaults = [
+    { name: 'Oatmeal', color: '#E2E2E2' },
+    { name: 'White', color: '#FFF' },
+];
+const borderDefaults = [
+    { name: 'Oatmeal', color: '#E2E2E2' },
+    { name: 'Oatmeal (dark)', color: '#D5D5CD' },
+    { name: 'Gray', color: '#D8D8D8' },
+    { name: 'Black', color: '#000' },
+];
 
 const edit = ({ attributes, className, setAttributes, isSelected }) => {
-    const { width } = attributes;
+    const { width, borderColor, bgColor } = attributes;
     const { toggleSelection } = dispatch('core/block-editor');
     return (
-        <div className={className}>
-            <ResizableBox
-                size={{
-                    width,
-                }}
-                minWidth="200"
-                maxWidth="640"
-                enable={{
-                    top: false,
-                    right: isSelected,
-                    bottom: false,
-                    left: false,
-                    topRight: false,
-                    bottomRight: false,
-                    bottomLeft: false,
-                    topLeft: false,
-                }}
-                onResizeStop={(event, direction, elt, delta) => {
-                    setAttributes({
-                        width: parseInt(width + delta.width, 10),
-                    });
-                    toggleSelection(true);
-                }}
-                onResizeStart={() => {
-                    toggleSelection(false);
-                }}
-            >
-                <InnerBlocks
-                    allowedBlocks={ALLOWED}
-                    template={TEMPLATE}
-                    templateLock="all"
-                />
-            </ResizableBox>
-        </div>
+        <Fragment>
+            <InspectorControls>
+                <PanelBody title={__('Flip Card Options')}>
+                    <PanelRow>
+                        <TextControl
+                            label="Width"
+                            value={width}
+                            onChange={w => setAttributes({ width: w })}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <div>
+                            <label
+                                style={{
+                                    marginBottom: '0.5em',
+                                    display: 'inline-block',
+                                }}
+                            >
+                                Border Color
+                            </label>
+                            <ColorPalette
+                                colors={borderDefaults}
+                                value={borderColor}
+                                onChange={c =>
+                                    setAttributes({ borderColor: c })
+                                }
+                                disableCustomColors
+                            />
+                        </div>
+                    </PanelRow>
+                    <PanelRow>
+                        <div>
+                            <label
+                                style={{
+                                    marginBottom: '0.5em',
+                                    display: 'inline-block',
+                                }}
+                            >
+                                Background Color
+                            </label>
+                            <ColorPalette
+                                colors={bgDefaults}
+                                value={bgColor}
+                                onChange={c => setAttributes({ bgColor: c })}
+                                disableCustomColors
+                            />
+                        </div>
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+            <div className={className}>
+                <ResizableBox
+                    size={{
+                        width,
+                    }}
+                    minWidth="200"
+                    maxWidth="640"
+                    enable={{
+                        top: false,
+                        right: isSelected,
+                        bottom: false,
+                        left: false,
+                        topRight: false,
+                        bottomRight: false,
+                        bottomLeft: false,
+                        topLeft: false,
+                    }}
+                    onResizeStop={(event, direction, elt, delta) => {
+                        setAttributes({
+                            width: parseInt(width + delta.width, 10),
+                        });
+                        toggleSelection(true);
+                    }}
+                    onResizeStart={() => {
+                        toggleSelection(false);
+                    }}
+                    style={{
+                        borderColor,
+                        backgroundColor: bgColor,
+                    }}
+                >
+                    <InnerBlocks
+                        allowedBlocks={ALLOWED}
+                        template={TEMPLATE}
+                        templateLock="all"
+                    />
+                </ResizableBox>
+            </div>
+        </Fragment>
     );
 };
 
