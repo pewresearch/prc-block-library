e60473f55703024039a36d862ceceba7898656d8
Shajia Abidi <sabidi@pewresearch.org>
2020-09-28 16:03:25 -0500

    updated the frontend to be the same for all the icons

--- src/social-toolbar/save.js
+++ src/social-toolbar/save.js
@@ -1,18 +1,5 @@
 const save = ({ attributes: { socialIcons } }) => {
     const returnIcon = icon => {
-        if ('facebook' === icon) {
-            return (
-                <div
-                    className="item fb-pew-share"
-                    fb-share-url="https://pewrsr.ch/3kOA1cN"
-                    fb-share-description=""
-                    style={{ cursor: 'pointer' }}
-                >
-                    <i className="facebook icon" />
-                </div>
-            );
-        }
-
         return (
             <div className="item" style={{ cursor: 'pointer' }}>
                 <i className={`icon ${icon}`} />
