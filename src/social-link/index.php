f37e4aaef5c5b2baf171fcbce94c23ac1828015f
Seth Rubenstein <seth@sethrubenstein.info>
2021-01-27 23:16:37 -0500

    Rewire social toolbar to use block

--- src/social-link/index.php
+++ src/social-link/index.php
@@ -14,6 +14,16 @@ class PRC_Social_Link extends PRC_Block_Library {
 	public function __construct( $init = false ) {
 		if ( true === $init ) {
 			add_action( 'init', array( $this, 'register_block' ), 11 );
+			// Ensure new frontend script is running
+			add_filter(
+				'prc_social_link_icon',
+				function( $markup, $attributes ) {
+					$this->enqueue_frontend_script();
+					return $markup;
+				},
+				10,
+				2
+			);
 		}
 	}
 
@@ -23,76 +33,6 @@ class PRC_Social_Link extends PRC_Block_Library {
 
 	public function render_as_button( $attributes ) {
 
-	}
-
-	public function get_menu_link( $attributes, $is_chiclet = false ) {
-		// Don't render the block's subtree if it has no label.
-		if ( empty( $attributes['label'] ) ) {
-			return '';
-		}
-
-		$is_active = ! empty( $attributes['id'] ) && ( get_the_ID() === $attributes['id'] );
-
-		$css_classes = ! empty( $attributes['className'] ) ? implode( ' ', (array) $attributes['className'] ) : false;
-
-		$wrapper_attributes = get_block_wrapper_attributes(
-			array(
-				'class' => ( $is_chiclet ? 'ui basic button ' : 'item ' ) . $css_classes . ( $is_active ? ' active' : '' ),
-			)
-		);
-
-		$html = '<a ' . $wrapper_attributes . ' ';
-
-		// Start appending HTML attributes to anchor tag.
-		if ( isset( $attributes['url'] ) ) {
-			$html .= ' href="' . esc_url( $attributes['url'] ) . '"';
-		}
-
-		if ( isset( $attributes['opensInNewTab'] ) && true === $attributes['opensInNewTab'] ) {
-			$html .= ' target="_blank"  ';
-		}
-
-		if ( isset( $attributes['rel'] ) ) {
-			$html .= ' rel="' . esc_attr( $attributes['rel'] ) . '"';
-		} elseif ( isset( $attributes['nofollow'] ) && $attributes['nofollow'] ) {
-			$html .= ' rel="nofollow"';
-		}
-
-		if ( isset( $attributes['title'] ) ) {
-			$html .= ' title="' . esc_attr( $attributes['title'] ) . '"';
-		}
-
-		// End appending HTML attributes to anchor tag.
-
-		// Start anchor tag content.
-		$html .= '>';
-
-		if ( isset( $attributes['label'] ) ) {
-			$html .= wp_kses(
-				$attributes['label'],
-				array(
-					'code'   => array(),
-					'em'     => array(),
-					'img'    => array(
-						'scale' => array(),
-						'class' => array(),
-						'style' => array(),
-						'src'   => array(),
-						'alt'   => array(),
-					),
-					's'      => array(),
-					'span'   => array(
-						'style' => array(),
-					),
-					'strong' => array(),
-				)
-			);
-		}
-
-		$html .= '</a>';
-		// End anchor tag content.
-
-		return $html;
 	}
 
 	public function enqueue_frontend_script() {
@@ -121,7 +61,6 @@ class PRC_Social_Link extends PRC_Block_Library {
 	 * @return string Returns the post content with the legacy widget added.
 	 */
 	public function render_menu_item_link( $attributes, $content, $block ) {
-		$this->enqueue_frontend_script();
 		// Don't render the block's subtree if it has no label.
 		if ( empty( $attributes['url'] ) ) {
 			return '';
