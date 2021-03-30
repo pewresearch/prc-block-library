9f21c3d0aabad92b427cb6c98d2c8b095f143efa
Seth Rubenstein <seth@sethrubenstein.info>
2021-03-30 11:38:06 -0400

    Final tweak on tabs before release

--- src/tabs/pane/edit.js
+++ src/tabs/pane/edit.js
@@ -8,11 +8,13 @@ import classnames from 'classnames';
  */
 import { Fragment } from '@wordpress/element';
 import {
+    InnerBlocks,
     useBlockProps,
     __experimentalUseInnerBlocksProps as useInnerBlocksProps,
 } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 
-const Edit = ({ attributes, className, context }) => {
+const Edit = ({ attributes, className, context, clientId }) => {
     const { uuid } = attributes;
     // eslint-disable-next-line react/destructuring-assignment
     const currentlyActive = context['prc-block/tabs-active'];
@@ -22,11 +24,25 @@ const Edit = ({ attributes, className, context }) => {
         return <Fragment />;
     }
 
+    const { hasChildBlocks } = useSelect(
+        select => {
+            const { getBlockOrder } = select('core/block-editor');
+            return {
+                hasChildBlocks: 0 < getBlockOrder(clientId).length,
+            };
+        },
+        [clientId],
+    );
+
     const blockProps = useBlockProps({
         className: classnames(className),
     });
 
-    const innerBlocksProps = useInnerBlocksProps({}, {});
+    const innerBlocksProps = useInnerBlocksProps(blockProps, {
+        renderAppender: hasChildBlocks
+            ? InnerBlocks.DefaultBlockAppender
+            : InnerBlocks.ButtonBlockAppender,
+    });
 
     return (
         <div {...blockProps}>
