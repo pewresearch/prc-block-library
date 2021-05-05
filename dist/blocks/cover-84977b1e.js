59bfb0a1f3f71e2cfee720b8f6cfbc78e0f410f8
Seth Rubenstein <srubenstein@pewresearch.org>
2021-05-04 23:20:48 -0400

    Getting alt card style working

--- dist/blocks/cover-84977b1e.js
+++ dist/blocks/cover-84977b1e.js
@@ -16,4 +16,4 @@
  * A zero setup Webpack Bundler Script for WordPress
  */
 (window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[5],{2:function(e,s){e.exports=wp.i18n},20:function(e,s,c){"use strict";var r="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");c.p=window["__wpackIo".concat(r)]},245:function(e,s,c){c(20),e.exports=c(246)},246:function(e,s,c){"use strict";c.r(s);var r=c(2),o=c(9);c(247);Object(o.registerBlockStyle)("core/cover",[{name:"essay",label:"Essay Header"}]),Object(o.registerBlockVariation)("core/cover",{name:"essay-header",title:Object(r.__)("Essay Header"),description:Object(r.__)("A standard essay style header."),attributes:{className:"is-style-essay"},innerBlocks:[["core/heading"]]})},247:function(e,s,c){},9:function(e,s){e.exports=wp.blocks}},[[245,0]]]);
-//# sourceMappingURL=cover-1dee86e2.js.map
+//# sourceMappingURL=cover-84977b1e.js.map
