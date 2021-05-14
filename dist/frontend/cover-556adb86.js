a936f3f464bcb7963f721d0379d8a3158f013586
Seth Rubenstein <srubenstein@pewresearch.org>
2021-05-13 23:14:39 -0400

    Clean block lib build

---
+++ dist/frontend/cover-556adb86.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryfrontendJsonp=window.wpackioprcBlocksLibraryfrontendJsonp||[]).push([[5],{19:function(e,n){e.exports=wp.domReady},35:function(e,n,o){"use strict";var t="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");o.p=window["__wpackIo".concat(t)]},476:function(e,n,o){o(35),e.exports=o(477)},477:function(e,n,o){"use strict";o.r(n);var t=o(19),i=o.n(t),r=(o(478),function(){var e=document.querySelectorAll(".wp-block-cover.is-style-essay"),n=document.documentElement.clientWidth;e.forEach((function(e){var o,t,i=e.querySelector(".wp-block-cover__inner-container");741<=n&&!e.querySelector(".wp-block-cover__inner-container")&&(e.nextElementSibling.classList.remove("is-style-mobile-essay-header"),e.appendChild(e.nextElementSibling)),740>=n&&null!==i&&(i.classList.toggle("is-style-mobile-essay-header"),o=i,(t=e).parentNode.insertBefore(o,t.nextSibling))}))});window.addEventListener("resize",r),i()((function(){r()}))},478:function(e,n,o){}},[[476,0]]]);
+//# sourceMappingURL=cover-556adb86.js.map
---
+++ dist/frontend/tabs-controller-7cc3fd2e.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryfrontendJsonp=window.wpackioprcBlocksLibraryfrontendJsonp||[]).push([[10],{19:function(t,e){t.exports=wp.domReady},30:function(t,e){t.exports=wp.url},35:function(t,e,c){"use strict";var o="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");c.p=window["__wpackIo".concat(o)]},488:function(t,e,c){c(35),t.exports=c(489)},489:function(t,e,c){"use strict";c.r(e);var o=c(19),n=c.n(o),r=c(30);n()((function(){document.querySelectorAll(".wp-block-prc-block-tabs").forEach((function(t){var e=t.getAttribute("id"),c=t.classList.contains("accordion");t.querySelectorAll(".wp-block-prc-block-tabs-menu-item").forEach((function(t){var o=t.getAttribute("data-uuid");t.classList;t.addEventListener("click",(function(){!function(t){var e=document.getElementById(t).querySelector(".wp-block-prc-block-tabs-menu-item.active");e&&e.classList.remove("active");var c=document.getElementById(t).querySelector(".wp-block-prc-block-tabs-pane.active");c&&c.classList.remove("active")}(e),function(t,e,c){console.log("makeActive",t,e,c);var o=document.querySelector('.wp-block-prc-block-tabs-menu-item[data-uuid="'.concat(t,'"]'));o&&(c?(console.log("menuItem",o),o.classList.toggle("active")):(console.log("not accordion"),o.classList.add("active")));var n=document.querySelector('.wp-block-prc-block-tabs-pane[data-uuid="'.concat(t,'"]'));if(n){var a={menuItem:t},i=Object(r.addQueryArgs)(window.location.href,a);c&&!0===n.classList.contains("active")?(i=Object(r.removeQueryArgs)(i,"menuItem"),n.classList.remove("active")):n.classList.add("active"),window.history.pushState(a,document.title,i)}}(o,e,c)}))}))})),function(){var t=Object(r.getQueryArg)(window.location.href,"menuItem");if(t){var e=document.querySelector('.wp-block-prc-block-tabs-menu-item[data-uuid="'.concat(t,'"]'));setTimeout((function(){return e.scrollIntoView(!0)}),1e3)}}()}))}},[[488,0]]]);
+//# sourceMappingURL=tabs-controller-7cc3fd2e.js.map
