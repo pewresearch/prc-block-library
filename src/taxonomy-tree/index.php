b1a02f4ea0cdabd041d89459e0f6303bf21f52df
Seth Rubenstein <srubenstein@pewresearch.org>
2021-05-21 17:00:12 -0400

    Link styles

--- src/taxonomy-tree/index.php
+++ src/taxonomy-tree/index.php
@@ -38,7 +38,7 @@ class Taxonomy_Tree extends PRC_Block_Library {
 		ob_start();
 		?>
 		<<?php echo esc_attr( $tag_name ); ?> class="<?php echo esc_attr( $classnames ); ?>"><?php echo filter_block_kses_value( $sub_heading, 'post' ); ?></<?php echo esc_attr( $tag_name ); ?>>
-		<div class="ui relaxed list" data-blockName="prc-block/taxonomy-tree">
+		<div class="ui relaxed link list" data-blockName="prc-block/taxonomy-tree">
 			<?php echo wp_kses( $content, 'post' ); ?>
 		</div>
 		<?php
