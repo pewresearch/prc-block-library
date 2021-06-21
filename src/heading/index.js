5ca3dab2d951dffae7ad69205f146e33dd40ce38
Seth Rubenstein <srubenstein@pewresearch.org>
2021-06-21 16:38:18 -0400

    refactor(prc-parent-theme): :recycle: TOC is now comprised of a series of interchangeable react functional components. New default level in core/heading and new isChapter attr.
    Refactored the TOC component into functional react components. Also fixed the sticky by changing the pushable css class to only apply an transform when active, fixing sticky issues.
    
    1498

--- src/heading/index.js
+++ src/heading/index.js
@@ -6,11 +6,37 @@
     registerBlockStyle,
     registerBlockVariation
 } from '@wordpress/blocks';
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal Dependencies
  */
 import './style.scss';
+
+/**
+ * Modify default settings on core/heading block. Change the default heading level to 4 and add isChapter attribute to replace prc-block/chapter at a later date.
+ * 
+ * @param {*} settings 
+ * @param {*} name 
+ * @returns 
+ */
+function modifyDefaultSettings( settings, name ) {
+    if ( 'core/heading' !== name ) {
+        return settings;    
+    }
+    settings.attributes.level.default = 4;
+    settings.attributes.isChapter = {
+        type: 'boolean',
+        default: false,
+    };
+	return settings;
+}
+
+addFilter(
+    'blocks.registerBlockType',
+    'prc-block-library/heading',
+    modifyDefaultSettings,
+);
 
 registerBlockStyle('core/heading', [
     {
@@ -26,11 +52,9 @@ registerBlockStyle('core/heading', [
 registerBlockVariation('core/heading', {
     name: 'section-header',
     title: __('Section Header'),
-    description: __('A heading styled for section headings'),
+    description: __('A heading styled for "section headings".'),
     attributes: {
         className: 'is-style-section-header',
         level: 3,
     },
 });
-
-// @TODO: Add support for a new attribute, isChapter and add support for a php class implementation of the new Gutenberg Table of Contents functionality. We can replace our custom table of contents functionality which would be great.
