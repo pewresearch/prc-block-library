d4d0c1d4b5aa4e4ecccc3b714e82503f88533c15
Seth Rubenstein <srubenstein@pewresearch.org>
2021-07-23 15:50:07 -0400

    Starting work on a github gist block

---
+++ src/github-gist/controls.js
@@ -0,0 +1,44 @@
+/**
+ * WordPress Dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
+import { BlockControls } from '@wordpress/block-editor';
+import { Toolbar } from '@wordpress/components';
+import { edit } from '@wordpress/icons';
+
+class Controls extends Component {
+    render() {
+        const {
+            preview,
+            setState,
+        } = this.props;
+
+        const editControl = [
+            {
+                icon: edit,
+                title: preview
+                    ? sprintf(
+                        /* translators: %s: "Gist", the name of a code sharing platform */
+                        __( 'Return to %s', 'coblocks' ),
+                        'Gist'
+                    )
+                    : sprintf(
+                        /* translators: %s: "Gist", the name of a code sharing platform */
+                        __( 'Edit %s URL', 'coblocks' ),
+                        'Gist'
+                    ),
+                onClick: () => setState( { preview: ! preview } ),
+                isActive: ! preview,
+            },
+        ];
+
+        return (
+            <BlockControls>
+                <Toolbar controls={ editControl } />
+            </BlockControls>
+        );
+    }
+}
+
+export default Controls;
