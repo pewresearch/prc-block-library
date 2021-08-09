e52a0478fd19bcf04ed52563fd53b1368cb7e5ac
Ben Wormald <benwormald@gmail.com>
2021-08-09 16:05:14 -0400

    add inspector controls for table

--- src/chart-builder-data-wrapper/edit/index.js
+++ src/chart-builder-data-wrapper/edit/index.js
@@ -2,12 +2,20 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { BlockControls, InnerBlocks } from '@wordpress/block-editor';
-import { ToolbarButton, ToolbarGroup } from '@wordpress/components';
+import {
+    BlockControls,
+    InnerBlocks,
+    InspectorControls,
+} from '@wordpress/block-editor';
+import {
+    ToolbarButton,
+    ToolbarGroup,
+    ToggleControl,
+} from '@wordpress/components';
 import { minus, plusCircleFilled } from '@wordpress/icons';
 import { select, dispatch } from '@wordpress/data';
 
-const edit = ({ attributes, setAttributes, clientId }) => {
+const edit = ({ attributes, setAttributes, clientId, isSelected }) => {
     console.log({ attributes, clientId });
     // Create an empty table + chart component on page load
     const MY_TEMPLATE = [
@@ -29,6 +37,39 @@ const edit = ({ attributes, setAttributes, clientId }) => {
 
     return (
         <>
+            <InspectorControls>
+                <ToggleControl
+                    label={__('Toggle table visbility')}
+                    help={
+                        attributes.hideTable
+                            ? 'Table hidden.'
+                            : 'Table visible.'
+                    }
+                    checked={attributes.hideTable}
+                    onChange={() => {
+                        const tableBlock = select('core/block-editor')
+                            .getBlocks(clientId)
+                            .find((block) => block.name === 'core/table');
+                        if (attributes.hideTable) {
+                            dispatch('core/editor').updateBlockAttributes(
+                                tableBlock.clientId,
+                                {
+                                    className: 'chart-builder-data-table',
+                                },
+                            );
+                        } else {
+                            dispatch('core/editor').updateBlockAttributes(
+                                tableBlock.clientId,
+                                {
+                                    className:
+                                        'chart-builder-data-table--hidden',
+                                },
+                            );
+                        }
+                        setAttributes({ hideTable: !attributes.hideTable });
+                    }}
+                />
+            </InspectorControls>
             <BlockControls>
                 <ToolbarGroup>
                     <ToolbarButton
