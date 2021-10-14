282ce3e2f38a0300d2fccbcfac59a173a7c9ca5b
Shajia Abidi <sabidi@pewresearch.org>
2021-10-14 13:38:40 -0500

    updates styles added chart builder removed journalism folder from theme

---
+++ dist/blocks/heading-e3a9b7a7.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.0.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[15],{122:function(e,t){e.exports=window.wp.hooks},2:function(e,t){e.exports=window.wp.i18n},23:function(e,t,o){var i="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");o.p=window["__wpackIo".concat(i)]},598:function(e,t,o){o(23),e.exports=o(710)},710:function(e,t,o){"use strict";o.r(t);var i=o(2),r=o(9),c=o(122);Object(c.addFilter)("blocks.registerBlockType","prc-block-library/heading",(function(e,t){return"core/heading"!==t||(e.attributes.level.default=4,e.attributes.isChapter={type:"boolean",default:!1}),e})),Object(r.registerBlockStyle)("core/heading",[{name:"section-header",label:"Section Header"},{name:"sub-header",label:"Sub Header"}]),Object(r.registerBlockVariation)("core/heading",{name:"section-header",title:Object(i.__)("Section Header"),description:Object(i.__)('A heading styled for "section headings".'),attributes:{className:"is-style-section-header",level:3}})},9:function(e,t){e.exports=window.wp.blocks}},[[598,0]]]);
+//# sourceMappingURL=heading-e3a9b7a7.js.map
