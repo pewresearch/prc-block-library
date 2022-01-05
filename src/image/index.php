3ccad1c32da75192de444655fafe39588e05d243
Seth Rubenstein <srubenstein@pewresearch.org>
2022-01-05 11:15:11 -0500

    Attempt to use new registered style in place of core image

--- src/image/index.php
+++ src/image/index.php
@@ -4,45 +4,34 @@ use \WPackio as WPackio;
 class Image extends PRC_Block_Library {
 	public function __construct( $init = false ) {
 		if ( true === $init ) {
-			add_action('wp_enqueue_scripts', array( $this, 'deregister_core_style' ) );
-			add_action( 'wp_head', array($this, 'deregister_core_styles'), 1 );
-			add_action( 'wp_footer', array($this, 'deregister_core_styles'), 1 );
+			add_action( 'admin_enqueue_scripts', array( $this, 'register_assets' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ) );
 		}
 	}
 
-	public function deregister_core_style() {
+	public function register_assets() {
 		wp_deregister_style( 'wp-block-image' );
-	}
 
-	// Basing logic on how to hook in here https://github.com/WordPress/gutenberg/blob/trunk/lib/blocks.php#L284-L313.
-	public function deregister_core_styles() {
-		global $wp_styles;
+		$ver = parent::$version;
+		$enqueue = new WPackio( 'prcBlocksLibrary', 'dist', $ver, 'plugin', parent::$plugin_file );
 
-		// Build an array of styles that have a path defined.
-		foreach ( $wp_styles->queue as $handle ) {
-			if ( wp_styles()->get_data( $handle, 'path' ) && file_exists( $wp_styles->registered[ $handle ]->extra['path'] ) ) {
-				do_action('qm/debug', print_r($wp_styles->registered[ $handle ], true) );
-				// We'll need to do a wp_register_style() for this one.
-				// Then we'll need to add a new style with the same handle pointing to our own file.
-			}
-		}
-	}
+		$registered = $enqueue->register(
+			'blocks',
+			'image',
+			array(
+				'js'        => false,
+				'css'       => true,
+				'js_dep'    => array(),
+				'css_dep'   => array(),
+				'in_footer' => true,
+				'media'     => 'all',
+			)
+		);
 
-	public function register_new_styles() {
-		$enqueue = new WPackio( 'prcBlocksLibrary', 'dist', parent::$version, 'plugin', parent::$plugin_file );
+		$handle = array_pop( $registered['js'] )['handle'];
+		$src = array_pop( $registered['css'] )['url'];
 
-		// $registered = $enqueue->register(
-		// 	'blocks',
-		// 	'image',
-		// 	array(
-		// 		'js'        => false,
-		// 		'css'       => true,
-		// 		'js_dep'    => array(),
-		// 		'css_dep'   => array(),
-		// 		'in_footer' => true,
-		// 		'media'     => 'all',
-		// 	)
-		// );
+		wp_register_style( 'wp-block-image', $src,  array(), $ver );
 	}
 }
 
