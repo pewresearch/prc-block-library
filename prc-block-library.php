932dd4350c79076bd58e8135fb1314e27f63f03f
Seth Rubenstein <srubenstein@pewresearch.org>
2022-01-30 17:00:39 -0500

    Version bumps

--- prc-block-library.php
+++ prc-block-library.php
@@ -11,7 +11,7 @@
  * Plugin Name:       PRC Block Library
  * Plugin URI:        https://pewresearch.org
  * Description:       PRC Block Library
- * Version:           2.1.13a
+ * Version:           2.1.13b
  * Requires at least: 5.4
  * Requires PHP:      7.4
  * Author:            Seth Rubenstein, Benjamin Wormald
@@ -42,7 +42,7 @@ class PRC_Block_Library {
 	 *
 	 * @var string
 	 */
-	public static $version = '2.1.13a';
+	public static $version = '2.1.13b';
 
 	/**
 	 * Registered wpackio assets
