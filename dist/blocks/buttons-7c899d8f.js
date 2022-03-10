d60fa3a57b2f31c332c246e8caacb7549c8ed7d2
Seth Rubenstein <srubenstein@pewresearch.org>
2022-03-10 18:53:18 -0500

    Merge RLS changes and re-build block library

---
+++ dist/blocks/buttons-7c899d8f.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.9
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[5],{24:function(o,c,i){var n="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");i.p=window["__wpackIo".concat(n)]},732:function(o,c,i){i(24),o.exports=i(733)},733:function(o,c){}},[[732,0]]]);
+//# sourceMappingURL=buttons-7c899d8f.js.map
---
+++ dist/blocks/query-1cbce404.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.9
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[38],{12:function(e,t){e.exports=window.wp.blocks},24:function(e,t,r){var o="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");r.p=window["__wpackIo".concat(o)]},924:function(e,t,r){r(24),e.exports=r(925)},925:function(e,t,r){"use strict";r.r(t);var o=r(12);Object(o.registerBlockVariation)("core/query",{name:"story-item-listing",title:"Story Item Listing",description:"Display a list of story items.",attributes:{query:{perPage:3,pages:1,offset:0,postType:"stub",categoryIds:[],tagIds:[],order:"desc",orderBy:"date",author:"",search:"",sticky:"exclude",inherit:!1}},innerBlocks:[["core/post-template",{},[["prc-block/story-item",{imageSlot:"left",imageSize:"A3"}]]]],scope:["block"]}),Object(o.unregisterBlockVariation)("core/query","posts-list"),Object(o.unregisterBlockVariation)("core/query","title-date"),Object(o.unregisterBlockVariation)("core/query","title-excerpt"),Object(o.unregisterBlockVariation)("core/query","title-date-excerpt"),Object(o.unregisterBlockVariation)("core/query","image-date-title")}},[[924,0]]]);
+//# sourceMappingURL=query-1cbce404.js.map
