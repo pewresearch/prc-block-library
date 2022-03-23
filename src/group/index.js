56483e69c206db7cc29b6dfe7562b5218b8395d5
Seth Rubenstein <srubenstein@pewresearch.org>
2022-03-23 12:32:58 -0400

    Set new maximum for responsive threshold based on device usage data from GA

--- src/group/index.js
+++ src/group/index.js
@@ -216,9 +216,12 @@ const GroupBlockAdvancedControls = createHigherOrderComponent(
 							label={__('Responsive Threshold')}
 							value={responsiveThreshold}
 							onChange={(val) => setAttributes({ responsiveThreshold: val })}
-							max={2000}
+							max={3540}
 							min={320}
 							isDragEnabled
+							help={__(
+								`The responsive threshold is the point at which the group block and its contents will trigger their mobile behavior. The default is 640px (small tablet), but you can change this to any value you like. If you would like to trigger the mobile behavior of a block immediately regardless of device size then use the maximum value of 3540.`,
+							)}
 						/>
 						<CardDivider />
 					</InspectorAdvancedControls>
