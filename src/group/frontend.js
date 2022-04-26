97cca185ee7973e0a622d6271eb52274704592db
Seth Rubenstein <srubenstein@pewresearch.org>
2022-04-26 18:58:31 -0400

    #4.1.0 Needs some additional mobile tweaking

--- src/group/frontend.js
+++ src/group/frontend.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * External Dependencies
  */
@@ -8,7 +9,7 @@ import enquire from 'enquire.js';
  */
 import domReady from '@wordpress/dom-ready';
 
-if ( !window.hasOwnProperty('prcBlocks') ) {
+if (!window.hasOwnProperty('prcBlocks')) {
 	window.prcBlocks = {};
 }
 window.prcBlocks.groupBlocks = {
@@ -20,21 +21,21 @@ const initStickyBlocks = () => {
 	const stickyBlocks = document.querySelectorAll('.prc-group-block--sticky');
 	// If there are any sticky blocks init them.
 	if (stickyBlocks.length) {
-		stickyBlocks.forEach(block => {
+		stickyBlocks.forEach((block) => {
 			block.classList.add('sticky');
 		});
 	}
-}
+};
 
 const initResponsiveGroupBlocks = () => {
 	// Helper Functions:
 	const addResponsiveBlock = (block, attachBlock, threshold) => {
 		console.log(window.prcBlocks.groupBlocks.breakpoints, threshold);
-		if(!window.prcBlocks.groupBlocks.breakpoints.includes(threshold)) {
+		if (!window.prcBlocks.groupBlocks.breakpoints.includes(threshold)) {
 			window.prcBlocks.groupBlocks.breakpoints.push(threshold);
 		}
 		console.log(window.prcBlocks.groupBlocks.elms, threshold);
-		if(!window.prcBlocks.groupBlocks.elms.hasOwnProperty(threshold)) {
+		if (!window.prcBlocks.groupBlocks.elms.hasOwnProperty(threshold)) {
 			window.prcBlocks.groupBlocks.elms[threshold] = [];
 		}
 		window.prcBlocks.groupBlocks.elms[threshold].push(block);
@@ -44,52 +45,54 @@ const initResponsiveGroupBlocks = () => {
 		const returnId = block.getAttribute('data-return-id');
 		returnPoint.setAttribute('id', returnId);
 		console.log('addResponsiveBlock', block, returnPoint);
-		block.parentNode.insertBefore(returnPoint, block.nextSibling)
-	}
+		block.parentNode.insertBefore(returnPoint, block.nextSibling);
+	};
 
 	const matchResponsiveBlock = (viewportWidth) => {
-		window.prcBlocks.groupBlocks.elms[viewportWidth].forEach(block => {
+		window.prcBlocks.groupBlocks.elms[viewportWidth].forEach((block) => {
 			// We should remove the sticky class if it exists.
 			const attachId = block.getAttribute('data-attach-id');
 			const attachBlock = document.getElementById(attachId);
-			if ( attachBlock ){
+			if (attachBlock) {
 				attachBlock.append(block);
 			}
 		});
-	}
+	};
 
 	const unmatchResponsiveBlock = (viewportWidth) => {
-		window.prcBlocks.groupBlocks.elms[viewportWidth].forEach(block => {
+		window.prcBlocks.groupBlocks.elms[viewportWidth].forEach((block) => {
 			const returnId = block.getAttribute('data-return-id');
 			const returnPoint = document.getElementById(returnId);
 			// If the sticky class should be here re-add it.
-			if ( returnPoint ){
-				returnPoint.parentNode.insertBefore(block, returnPoint.nextSibling)
+			if (returnPoint) {
+				returnPoint.parentNode.insertBefore(block, returnPoint.nextSibling);
 			}
 		});
-	}
+	};
 
 	const watchResponsiveBlocks = () => {
 		console.log('watchResponsiveBlocks', window.prcBlocks.groupBlocks);
 
-		window.prcBlocks.groupBlocks.breakpoints.forEach(threshold => {
+		window.prcBlocks.groupBlocks.breakpoints.forEach((threshold) => {
 			// Register the enquire listener.
 			enquire.register(`screen and (max-width: ${threshold}px)`, {
-				match : () => {
+				match: () => {
 					console.log('match', threshold);
 					matchResponsiveBlock(threshold);
 				},
-				unmatch : () => {
+				unmatch: () => {
 					unmatchResponsiveBlock(threshold);
 				},
 			});
 		});
-	}
+	};
 
 	// If there are any resposnive blocks init them.
-	const responsiveBlocks = document.querySelectorAll('.prc-group-block--responsive');
+	const responsiveBlocks = document.querySelectorAll(
+		'.prc-group-block--responsive',
+	);
 	if (responsiveBlocks.length) {
-		responsiveBlocks.forEach(block => {
+		responsiveBlocks.forEach((block) => {
 			const attachId = block.getAttribute('data-attach-id');
 			const threshold = block.getAttribute('data-responsive-threshold');
 
@@ -102,7 +105,7 @@ const initResponsiveGroupBlocks = () => {
 		// Watch for changes in the viewport.
 		watchResponsiveBlocks();
 	}
-}
+};
 
 domReady(() => {
 	initResponsiveGroupBlocks();
