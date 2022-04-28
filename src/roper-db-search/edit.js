b324e9ac5fbc3565ffa951810ed3054e01418dde
Seth Rubenstein <srubenstein@pewresearch.org>
2022-04-28 16:51:24 -0400

    #4.1.0 Pushing up some caching tweaks

--- src/roper-db-search/edit.js
+++ src/roper-db-search/edit.js
@@ -6,21 +6,70 @@ import classnames from 'classnames';
 /**
  * WordPress Dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import {
+	BaseControl,
+	SelectControl,
+	TextControl,
+	__experimentalNumberControl as NumberControl,
+	PanelBody,
+	Placeholder,
+} from '@wordpress/components';
 
 const edit = ({ attributes, className, setAttributes }) => {
 	const { subText, perPage, type } = attributes;
+
+	// block controls for subText, perPage, and type
 
 	const blockProps = useBlockProps({
 		className: classnames(className),
 	});
 
 	return (
-		<div {...blockProps}>
-			<p>Roper Db Search</p>
-		</div>
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={__('Roper DB Settings')}>
+					<TextControl
+						label={__('Sub Text')}
+						value={subText}
+						onChange={(v) => setAttributes({ subText: v })}
+					/>
+					<BaseControl label={__('Per Page')}>
+						<NumberControl
+							value={perPage}
+							min={5}
+							max={20}
+							onChange={(v) => setAttributes({ perPage: v })}
+						/>
+					</BaseControl>
+					<SelectControl
+						label={__('Type')}
+						value={type}
+						options={[
+							{ label: 'Default', value: 'default' },
+							{ label: 'Global', value: 'global' },
+						]}
+						onChange={(v) => setAttributes({ type: v })}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div {...blockProps}>
+				<Placeholder
+					icon="database"
+					label={__(' Roper DB Search')}
+					instructions={__(
+						'This block will render the Roper Database search on the frontend. You can edit the settings in the block inspector.',
+					)}
+				>
+					<p>
+						Will render <pre style={{ display: 'inline' }}>{type}</pre> Roper
+						Search
+					</p>
+				</Placeholder>
+			</div>
+		</Fragment>
 	);
 };
 
