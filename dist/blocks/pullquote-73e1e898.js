bb1f723fced966466943a5fd5eca2911575c9750
benwormald <benwormald@gmail.com>
2022-05-17 16:26:41 -0400

    fresh library build

---
+++ dist/blocks/pullquote-73e1e898.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.14
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[39],{23:function(c,o,r){var i="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");r.p=window["__wpackIo".concat(i)]},712:function(c,o,r){r(23),c.exports=r(820)},820:function(c,o,r){"use strict";r.r(o)}},[[712,0]]]);
+//# sourceMappingURL=pullquote-73e1e898.js.map
---
+++ dist/blocks/social-link-3875386b.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.14
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[45],{2:function(t,e){t.exports=window.wp.i18n},23:function(t,e,n){var o="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");n.p=window["__wpackIo".concat(o)]},25:function(t,e){t.exports=window.wp.primitives},3:function(t,e){t.exports=window.wp.components},4:function(t,e){t.exports=window.wp.element},5:function(t,e){t.exports=window.wp.blockEditor},719:function(t,e,n){n(23),t.exports=n(720)},720:function(t,e,n){"use strict";n.r(e);var o=n(2),c=n(4),i=n(84),r=n(91),a=n(5),l=n(3),s=(n(9),n(25),Object(i.createHigherOrderComponent)((function(t){return function(e){var n=e.name,i=e.attributes,r=e.setAttributes;if("core/social-links"!==n)return React.createElement(t,e);var s=i.title,p=i.description;return React.createElement(c.Fragment,null,React.createElement(a.InspectorControls,null,React.createElement(l.PanelBody,{title:Object(o.__)("Title and Description")},React.createElement(l.TextControl,{label:Object(o.__)("Title"),value:s,onChange:function(t){return r({title:t})}}),React.createElement(l.TextControl,{label:Object(o.__)("Description"),value:p,onChange:function(t){return r({description:t})}}))),React.createElement(t,e))}}),"withSocialLinksExtraControls"));Object(r.addFilter)("editor.BlockEdit","prc-block/social-links",s,21)},84:function(t,e){t.exports=window.wp.compose},9:function(t,e){t.exports=window.wp.blocks},91:function(t,e){t.exports=window.wp.hooks}},[[719,0]]]);
+//# sourceMappingURL=social-link-3875386b.js.map
