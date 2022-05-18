71cc78124da2fada351a070142c0b184f88f2c30
Seth Rubenstein <srubenstein@pewresearch.org>
2022-05-18 15:16:37 -0400

    #2886 Misc tweaks to TOC frontend: - Better handling for flipping on and off styles vis-a-vis mobile - Better scroll into view handling for clicking on toc elements

---
+++ dist/blocks/image-4a63ad84.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.14
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[20],{11:function(o,c){o.exports=window.wp.data},229:function(o,c){o.exports=window.wp.domReady},23:function(o,c,n){var r="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");n.p=window["__wpackIo".concat(r)]},681:function(o,c,n){n(23),o.exports=n(805)},805:function(o,c,n){"use strict";n.r(c);var r=n(229),i=n.n(r),t=n(11),e=n(9);i()((function(){null!==Object(t.select)("core/editor")&&Object(e.unregisterBlockStyle)("core/image",["rounded"])}))},9:function(o,c){o.exports=window.wp.blocks}},[[681,0]]]);
+//# sourceMappingURL=image-4a63ad84.js.map
