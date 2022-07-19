881451acc59c0f6c50b37488286bf4df78476556
Seth Rubenstein <srubenstein@pewresearch.org>
2022-07-19 15:24:56 -0400

    Scroll carousel parent parent into view when we approach threshold

---
+++ dist/frontend/popup-controller-b78b619a.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.17
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryfrontendJsonp=window.wpackioprcBlocksLibraryfrontendJsonp||[]).push([[14],{29:function(o,n){o.exports=window.wp.domReady},30:function(o,n){o.exports=window.wp.url},40:function(o,n,t){var e="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");t.p=window["__wpackIo".concat(e)]},876:function(o,n,t){t(40),o.exports=t(877)},877:function(o,n,t){"use strict";t.r(n);var e=t(29),i=t.n(e),c=(t(30),!!window.hasOwnProperty("Vimeo")&&window.Vimeo),r=function(o){var n=!(arguments.length>1&&void 0!==arguments[1])||arguments[1],t=arguments.length>2&&void 0!==arguments[2]?arguments[2]:{};!0===n?jQuery(o).modal("show"):jQuery(o).modal(t)};i()((function(){!function(){window.location.href;var o=document.querySelectorAll(".wp-block-prc-block-popup-controller");o.length&&o.forEach((function(o){var n=o.getAttribute("id"),t=o.querySelector(".wp-block-prc-block-popup-modal"),e=o.querySelector(".wp-block-prc-block-popup-content"),i={};t.setAttribute("data-trigger-id",n),(t=document.querySelector('.wp-block-prc-block-popup-modal[data-trigger-id="'.concat(n,'"]'))).getAttribute("data-transition")&&(i.transition=t.getAttribute("data-transition"));var a=t.classList.contains("is-style-video"),l=!1,d=!1;!0===a&&!1!==c&&c.hasOwnProperty("Player")&&(d=t.querySelector("iframe"),console.log("iframes found...",d),null!==d&&(d.setAttribute("allow","autoplay"),l=new c.Player(d))),1==t.getAttribute("data-inverted")&&(i.inverted=!0),!1!==l?(e.addEventListener("mouseenter",(function(){l.ready().then((function(){console.log("the player is ready")}))})),i.onVisible=function(){window.history.pushState("modal","Modal","#".concat(n)),l.play().then((function(){console.log("the video was played")})).catch((function(o){console.log("error...",o),o.name}))},i.onHidden=function(){window.history.back(),l.unload().then((function(){console.log("the video was unloaded")})).catch((function(o){o.name}))},l.on("play",(function(){console.log("played the video!")}))):(i.onVisible=function(){window.history.pushState("modal","Modal","#".concat(n))},i.onHidden=function(){window.history.back()}),r(t,!1,i),window.location.hash==="#".concat(n)&&(console.log("Auto hash..."),setTimeout((function(){r(t)}),500)),jQuery(e).on("click",(function(o){o.preventDefault(),r(t)}))}))}()}))}},[[876,0]]]);
+//# sourceMappingURL=popup-controller-b78b619a.js.map
--- src/carousel/frontend.js
+++ src/carousel/frontend.js
@@ -1,6 +1,5 @@
 /** External Dependencies */
 import { randomId } from '@prc-app/shared';
-import inViewport from 'in-viewport';
 
 /**
  * WordPress Dependencies
@@ -24,18 +23,11 @@ const resetAll = () => {
 	window.prcBlocks.carouselBlocks.activated = [];
 };
 
-function visible(elt) {
-	// elt === elem
-	console.log(elt.id + ' is visible in the window!');
-}
-
 domReady(() => {
 	const carousels = document.querySelectorAll('.wp-block-prc-block-carousel');
 
 	if (carousels.length) {
 		carousels.forEach((carousel) => {
-			inViewport(carousel, visible);
-
 			const ID = randomId();
 			// Assign a random id to each carousel
 			carousel.setAttribute('id', ID);
@@ -44,28 +36,27 @@ domReady(() => {
 				':scope > .wp-block-group:last-child',
 			);
 
-			inViewport(lastCarouselSlide, visible);
-
 			// Watch for scrolling on the document to activate the carousel.
 			window.addEventListener('scroll', () => {
 				const carouselTop = carousel.getBoundingClientRect().top;
 				const carouselHeight = carousel.getBoundingClientRect().height;
+
 				if (
 					0 >= carouselTop &&
 					-4 <= carouselTop &&
 					!window.prcBlocks.carouselBlocks.activated.includes(ID)
 				) {
+					carousel.parentElement.parentElement.scrollIntoView(); // Scroll to the carousel so its perfeclty in the viewport.
 					carousel.classList.add('active');
+					if (!window.prcBlocks.carouselBlocks.activated.includes(ID)) {
+						window.prcBlocks.carouselBlocks.activated.push(ID);
+					}
 
 					if (!window.location.hash) {
 						document.querySelector('body').classList.add('carousel-locked');
 					}
 
 					console.log('Carousel locked', carousel, carouselTop);
-
-					if (!window.prcBlocks.carouselBlocks.activated.includes(ID)) {
-						window.prcBlocks.carouselBlocks.activated.push(ID);
-					}
 				}
 
 				// If the carousel is scrolled to the bottom and out of view then reset the scroll position.
@@ -85,8 +76,10 @@ domReady(() => {
 			carousel.addEventListener('scroll', () => {
 				const lastCarouselSlideTop =
 					lastCarouselSlide.getBoundingClientRect().top;
+
+				console.log("Last Carousel Slide Location:", lastCarouselSlideTop);
 				if (
-					0 >= lastCarouselSlideTop &&
+					50 >= lastCarouselSlideTop &&
 					window.prcBlocks.carouselBlocks.activated.includes(ID)
 				) {
 					console.log('Carousel unlocked', ID);
