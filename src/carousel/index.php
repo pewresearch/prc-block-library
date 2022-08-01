04415c1b321ccc671614bec9607ad9ceb13cd110
Seth Rubenstein <srubenstein@pewresearch.org>
2022-08-01 16:01:33 -0400

    Version bump

--- src/carousel/index.php
+++ src/carousel/index.php
@@ -8,7 +8,7 @@ use \WPackio as WPackio;
  */
 
 class Carousel_Block extends PRC_Block_Library {
-	public static $version = '1.0.8e';
+	public static $version = '1.0.8f';
 
 	public function __construct( $init = false ) {
 		if ( true === $init ) {
