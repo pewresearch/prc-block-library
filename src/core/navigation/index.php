38706fd27cf5256f27935092ff1bdaa6445ab402
Seth Rubenstein <srubenstein@pewresearch.org>
2022-09-12 16:33:13 -0400

    Fixing nav block styles

--- src/core/navigation/index.php
+++ src/core/navigation/index.php
@@ -1,27 +1,20 @@
 <?php
 if ( function_exists( 'register_block_style' ) ) {
-	ob_start();
-	?>
-	.wp-block-navigation.is-style-pills .wp-block-navigation-item {
-		border: 1px solid #EAEAEA;
-		border-radius: 3px;
-		padding: .7857142857em 1.5em;
-		text-align: center;
-		text-decoration: none;
-		transition: opacity .1s ease, background-color .1s ease,color .1s ease, box-shadow .1s ease,background .1s ease;
-	}
-	.wp-block-navigation.is-style-pills .wp-block-navigation-item:hover {
-		background-color: #EAEAEA;
-	}
-	<?php
-	$style = ob_get_clean();
-    register_block_style(
-        'core/navigation',
-        array(
-            'name'         => 'pills',
-            'label'        => __( 'Pills', 'prc-block-library' ),
-            'inline_style' => $style,
-        )
-    );
+	add_action('after_setup_theme', function(){
+		wp_register_style(
+			'prc-block-library-core-navigation-styles',
+			plugins_url( 'style.css', __FILE__ ),
+			array(),
+			filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+		);
+		register_block_style(
+			'core/navigation',
+			array(
+				'name'   => 'pills',
+				'label'  => __( 'Pills', 'prc-block-library' ),
+				'style_handle'  => 'prc-block-library-core-navigation-styles'
+			)
+		);
+	});
 }
 
