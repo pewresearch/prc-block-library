3e17be154a425ac785c1ea4fcbe91d5cfae6c7b6
Seth Rubenstein <srubenstein@pewresearch.org>
2022-09-22 17:10:51 -0400

    Separator block

---
+++ dist/blocks/image-a9e9ccc6.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.17
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[22],{1060:function(o,c,n){"use strict";n.r(c);var r=n(246),i=n.n(r),t=n(9),e=n(8);i()((function(){null!==Object(t.select)("core/editor")&&Object(e.unregisterBlockStyle)("core/image",["rounded"])}))},22:function(o,c,n){var r="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");n.p=window["__wpackIo".concat(r)]},246:function(o,c){o.exports=window.wp.domReady},629:function(o,c,n){n(22),o.exports=n(1060)},8:function(o,c){o.exports=window.wp.blocks},9:function(o,c){o.exports=window.wp.data}},[[629,0]]]);
+//# sourceMappingURL=image-a9e9ccc6.js.map
--- src/core/separator/index.php
+++ src/core/separator/index.php
@@ -1,26 +1,60 @@
 <?php
+require_once PRC_VENDOR_DIR . '/autoload.php';
+use \WPackio as WPackio;
 
 class Core_Separator extends PRC_Block_Library {
-	public static $block_name = 'core/seperator';
+	public static $block_name = 'core/separator';
 	public static $version = '1.0.1';
 
 	public function __construct( $init = false ) {
 		if ( true === $init ) {
-			add_action( 'init', array($this, 'remove_styles') );
-			add_filter( 'render_block', array( $this, 'render_divider' ), 10, 2 );
+			add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_admin_assets' ) );
+			add_filter( 'render_block', array( $this, 'add_additional_frontend_styles' ), 10, 2 );
 		}
 	}
 
-	public function remove_styles() {
-		unregister_block_style( self::$block_name, 'wide');
+	public function enqueue_frontend_assets() {
+		$enqueue = new WPackio( 'prcBlocksLibrary', 'dist', parent::$version, 'plugin', parent::$plugin_file );
+		$proceed = is_admin() && get_current_screen()->is_block_editor || !is_admin();
+		// This is such a primitive block that it will affect styling in admin if ever accidentally called in for example the help system.
+		if ( $proceed ) {
+			$enqueue->enqueue(
+				'blocks',
+				'separator',
+				array(
+					'js'        => false,
+					'css'       => true,
+					'js_dep'    => array(),
+					'css_dep'   => array(),
+					'in_footer' => true,
+					'media'     => 'all',
+				)
+			);
+		}
 	}
 
-	public function render_divider( $block_content, $block ) {
-		if ( 'core/separator' !== $block['blockName'] ) {
-			return $block_content;
+	public function add_additional_frontend_styles( $block_content, $block ) {
+		if ( self::$block_name === $block['blockName'] ) {
+			$this->enqueue_frontend_assets();
 		}
-
 		return $block_content;
+	}
+
+	public function enqueue_admin_assets() {
+		$enqueue = new WPackio( 'prcBlocksLibrary', 'dist', self::$version, 'plugin', parent::$plugin_file );
+
+		$enqueue->enqueue(
+			'blocks',
+			'separator',
+			array(
+				'js'        => true,
+				'css'       => true,
+				'js_dep'    => array(),
+				'css_dep'   => array(),
+				'in_footer' => true,
+				'media'     => 'all',
+			)
+		);
 	}
 }
 
