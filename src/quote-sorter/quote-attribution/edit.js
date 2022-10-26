5b9ee8fb320848f735fe95f8008c28d7ad2d76e5
Seth Rubenstein <srubenstein@pewresearch.org>
2022-10-26 10:23:14 -0400

    Align control for attribution

--- src/quote-sorter/quote-attribution/edit.js
+++ src/quote-sorter/quote-attribution/edit.js
@@ -6,14 +6,28 @@ import classnames from 'classnames';
 /**
  * WordPress Dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { Fragment } from '@wordpress/element';
+import { useBlockProps, BlockControls, AlignmentControl } from '@wordpress/block-editor';
 
 const edit = ({ attributes, className, context }) => {
+	const { align } = attributes;
 	const attribution = context?.['prc-block/quote-sorter/attribution'];
 	const blockProps = useBlockProps({
 		className: classnames(className, 'attribution'),
+		[ `has-text-align-${ align }` ]: align,
 	});
-	return <div {...blockProps}>{attribution}</div>;
+
+	return (
+		<Fragment>
+				<BlockControls group="block">
+					<AlignmentControl
+						value={ align }
+						onChange={ ( newAlign ) => setAttributes( { align: newAlign } ) }
+					/>
+				</BlockControls>
+				<div {...blockProps}>{attribution}</div>
+			</Fragment>
+		);
 };
 
 export default edit;
