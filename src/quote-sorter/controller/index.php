d61fe0d2a7d79e5b62b39fc34002bae6e1f82640
Seth Rubenstein <srubenstein@pewresearch.org>
2022-10-31 17:39:36 -0400

    Merge complete

--- src/quote-sorter/controller/index.php
+++ src/quote-sorter/controller/index.php
@@ -212,12 +212,13 @@ class Quote_Sorter_Controller extends PRC_Block_Library {
 			)
 		);
 		$script_handle = array_pop( $registered['js'] )['handle'];
-		$style_handle = array_pop( $registered['css'] )['handle'];
+		// @TODO @benwormald is this necessary? It was throwing a warning bc no css file was found.
+		// $style_handle = array_pop( $registered['css'] )['handle'];
 		register_block_type_from_metadata(
 			plugin_dir_path( __DIR__ ) . '/controller',
 			array(
 				'editor_script'   => $script_handle,
-				'style' 		  => $style_handle,
+				// 'style' 		  => $style_handle,
 				'render_callback' => array( $this, 'render_block_callback' ),
 			)
 		);
