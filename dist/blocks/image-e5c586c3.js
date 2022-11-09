3e9fd7d4a1b219ab8be640c04813321e62f32ab7
Ben Wormald <benwormald@gmail.com>
2022-11-09 11:51:26 -0500

    #3164-b package bump to 2.1.22

---
+++ dist/blocks/image-e5c586c3.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.22
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[26],{13:function(o,c,n){var r="prcBlocksLibrarydist".replace(/[^a-zA-Z0-9_-]/g,"");n.p=window["__wpackIo".concat(r)]},187:function(o,c){o.exports=window.wp.domReady},508:function(o,c,n){n(13),o.exports=n(829)},7:function(o,c){o.exports=window.wp.blocks},829:function(o,c,n){"use strict";n.r(c);var r=n(187),i=n.n(r),t=n(9),e=n(7);i()((function(){null!==Object(t.select)("core/editor")&&Object(e.unregisterBlockStyle)("core/image",["rounded"])}))},9:function(o,c){o.exports=window.wp.data}},[[508,0]]]);
+//# sourceMappingURL=image-e5c586c3.js.map
---
+++ dist/blocks/social-share-url-field-c7947f5d.js
@@ -0,0 +1,24 @@
+/*!
+ * 
+ * prcBlocksLibrary
+ * 
+ * @author Seth Rubenstein
+ * @version 2.1.22
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2022 Seth Rubenstein
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackioprcBlocksLibraryblocksJsonp=window.wpackioprcBlocksLibraryblocksJsonp||[]).push([[59],{1:function(e,t){e.exports=window.React},16:function(e,t,r){var n,o=r(20);
+/*!
+  Copyright (c) 2018 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/!function(){"use strict";var i={}.hasOwnProperty;function c(){for(var e=[],t=0;t<arguments.length;t++){var r=arguments[t];if(r){var n=o(r);if("string"===n||"number"===n)e.push(r);else if(Array.isArray(r)){if(r.length){var s=c.apply(null,r);s&&e.push(s)}}else if("object"===n)if(r.toString===Object.prototype.toString)for(var a in r)i.call(r,a)&&r[a]&&e.push(a);else e.push(r.toString())}}return e.join(" ")}e.exports?(c.default=c,e.exports=c):"object"===o(r(19))&&r(19)?void 0===(n=function(){return c}.apply(t,[]))||(e.exports=n):window.classNames=c}()},2:function(e,t){e.exports=window.wp.i18n},357:function(e){e.exports=JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":2,"name":"prc-block/social-share-url-field","title":"Social Share URL Field","description":"A input field with a url that can be shared on social media. Clicking the field will copy the link.","icon":"admin-links","keywords":["social"],"category":"marketing","attributes":{"url":{"type":"string"}},"supports":{"html":false},"parent":["core/social-links"],"usesContext":["core/social-links/title","core/social-links/description","core/social-links/url"]}')},5:function(e,t){e.exports=window.wp.blockEditor},50:function(e,t){e.exports=window.ReactDOM},7:function(e,t){e.exports=window.wp.blocks},753:function(e,t,r){r(13),e.exports=r(803)},803:function(e,t,r){"use strict";r.r(t);var n=r(8),o=r(2),i=r(7),c=r(357),s=r(16),a=r.n(s),l=r(865),p=r(5),u=function(e){var t=e.attributes,r=e.className,n=e.setAttributes,i=e.context,c=t.url;console.log("Social Share URL Field: context: ",i);var s=Object(p.useBlockProps)({className:a()(r),style:{display:"flex",flexDirection:"row"}});return React.createElement("div",s,React.createElement("div",{className:"label"},Object(o.__)("Share This Link:","prc-block-library")),React.createElement(l.a,{placeholder:"URL...",value:c,onChange:function(e,t){var r=t.value;return n({url:r})}}))},f=function(){return React.createElement(p.InnerBlocks.Content,null)};function b(e,t){var r=Object.keys(e);if(Object.getOwnPropertySymbols){var n=Object.getOwnPropertySymbols(e);t&&(n=n.filter((function(t){return Object.getOwnPropertyDescriptor(e,t).enumerable}))),r.push.apply(r,n)}return r}function w(e){for(var t=1;t<arguments.length;t++){var r=null!=arguments[t]?arguments[t]:{};t%2?b(Object(r),!0).forEach((function(t){Object(n.a)(e,t,r[t])})):Object.getOwnPropertyDescriptors?Object.defineProperties(e,Object.getOwnPropertyDescriptors(r)):b(Object(r)).forEach((function(t){Object.defineProperty(e,t,Object.getOwnPropertyDescriptor(r,t))}))}return e}var d=c.name,h={edit:u,save:f};Object(i.registerBlockType)(d,w(w({},c),h))}},[[753,0,3]]]);
+//# sourceMappingURL=social-share-url-field-c7947f5d.js.map
