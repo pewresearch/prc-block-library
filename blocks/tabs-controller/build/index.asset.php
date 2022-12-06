eef8d9788b9e576d386d3952f70220d71cf9cc5a
Seth Rubenstein <srubenstein@pewresearch.org>
2022-12-06 15:14:37 -0500

    Completing chart builder merge

--- blocks/tabs-controller/build/index.asset.php
+++ blocks/tabs-controller/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f65c73ec695142f1586a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5e2bdf1c5be73a8a0b0d');
