86bf2606d362c79cdf80d761a55174070623db8f
Seth Rubenstein <srubenstein@pewresearch.org>
2022-12-14 13:59:02 -0500

    Wrapping up for today

---
+++ blocks/taxonomy-navigation-controller/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'cb3582f4c64220cde1e2');
---
+++ blocks/taxonomy-navigation-link/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a7431b2d53cc150b38e8');
