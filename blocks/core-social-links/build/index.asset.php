68551e37a53424a1fc1d888ca098e31657ada497
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-01 12:42:29 -0500

    Fix for social tools javascript, moving further toward block only

--- blocks/core-social-links/build/index.asset.php
+++ blocks/core-social-links/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '971c26efa18ab8fded22');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'cdce0627d366ef5f0a95');
--- blocks/core-social-links/build/view.asset.php
+++ blocks/core-social-links/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-url'), 'version' => '6bd6ab6e4c7a83952522');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-url'), 'version' => '78b9725eeb6b10d4d9b1');
