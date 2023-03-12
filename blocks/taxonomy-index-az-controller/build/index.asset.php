d4ce6fa609f13f94d8d6c2f7f956371936382b55
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-12 18:10:52 -0400

    Flip card fallover min width

--- blocks/taxonomy-index-az-controller/build/index.asset.php
+++ blocks/taxonomy-index-az-controller/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0adfdb82016da5698c15');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'cc4b19411ed7b41dd695');
