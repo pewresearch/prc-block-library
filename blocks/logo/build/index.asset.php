b5a7218eb2befa893513878384bbe9c27e094d35
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-15 22:02:49 -0400

    misc updates

--- blocks/logo/build/index.asset.php
+++ blocks/logo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c4c96ef387d0d6f243d8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0cc8812f3918d8163c29');
