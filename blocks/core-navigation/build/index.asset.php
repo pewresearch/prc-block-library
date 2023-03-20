a97d9c359ea18c980dd2ecb3df4d090332e545df
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-20 17:31:22 -0400

    Finally have a method for loading assets into the site editor

--- blocks/core-navigation/build/index.asset.php
+++ blocks/core-navigation/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => 'b5e3b660a694361546ef');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => '83a7af939e6c2ae8feaf');
