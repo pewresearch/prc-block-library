5c749f5f6dbd5b1d2ab55c659253e21dd72377bd
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-29 11:01:37 -0400

    Misc style tweaks

--- blocks/menu-link/build/index.asset.php
+++ blocks/menu-link/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('prc-components', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '91dad78c9c9bcf1c0bed');
+<?php return array('dependencies' => array('prc-components', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'f4b1f2ea7123782d0b9b');
