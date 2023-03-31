f38f09cb7a509d319d6410ed2ff920b23b15eb71
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-31 14:16:25 -0400

    Fresh build

--- blocks/tabs-controller/build/index.asset.php
+++ blocks/tabs-controller/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6d5a2bfbd3f4e1a7b68c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '67b0c5bf7e508b61222c');
