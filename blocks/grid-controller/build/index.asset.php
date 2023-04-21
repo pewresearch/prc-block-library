7c650c1b8c068318d38fee74bfc1a734453a81dd
Seth Rubenstein <srubenstein@pewresearch.org>
2023-04-21 12:32:18 -0400

    Grid updates

--- blocks/grid-controller/build/index.asset.php
+++ blocks/grid-controller/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8da9c5e3de667a0bcb1c');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '476be0498b8e347e2274');
