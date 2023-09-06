dcd701667edef4d2e21a557fa0a02d85a2f1e7e3
Seth Rubenstein <srubenstein@pewresearch.org>
2023-09-06 13:50:37 -0400

    Collapsible block hot links

--- blocks/collapsible/build/view.asset.php
+++ blocks/collapsible/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '5a5b4f07cc4cc8679bbd');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill', 'wp-url'), 'version' => 'b58c414e9fa0ac6959b4');
