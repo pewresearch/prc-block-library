0f4f9c635304d388dd6376026e66210f22f55a50
Seth Rubenstein <srubenstein@pewresearch.org>
2023-09-11 17:27:07 -0400

    Moving more plugins into composer and collapsing down prc-core

--- blocks/table-of-contents/build/index.asset.php
+++ blocks/table-of-contents/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('prc-icons', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '49108e65a142a6142121');
+<?php return array('dependencies' => array('prc-icons', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '89af2f39d0615a5f0503');
