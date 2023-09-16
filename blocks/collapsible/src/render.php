eaa812c4ba27280bac33bb920cdcff0bab2fe371
Seth Rubenstein <srubenstein@pewresearch.org>
2023-09-16 01:27:16 -0400

    Starting apple news block library integration

--- blocks/collapsible/src/render.php
+++ blocks/collapsible/src/render.php
@@ -7,11 +7,15 @@
 // $block (WP_Block): The block instance.
 
 $is_deprecated = array_key_exists('deprecated', $attributes) ? true : false;
-$title = array_key_exists( 'title', $attributes ) ? $attributes['title'] : 'How we did this';
+$collapsible_title = array_key_exists( 'title', $attributes ) ? $attributes['title'] : 'How we did this';
 
-echo wp_sprintf(
-	'<div %1$s><div class="wp-block-prc-block-collapsible__title"><div>%2$s</div><button class="wp-block-prc-block-collapsible__icon"><i aria-hidden="true" class="fa-light fa-circle-plus"></i><i aria-hidden="true" class="fa-light fa-circle-minus"></i></button></div><div class="wp-block-prc-block-collapsible__content">%3$s</div></div>',
-	$is_deprecated ? 'class="is-style-plus wp-block-prc-block-collapsible"' : get_block_wrapper_attributes(array('id' => sanitize_title($title))),
-	$title,
-	$content,
-);
+if ( function_exists( 'apple_news_is_exporting' ) && apple_news_is_exporting() ) {
+	echo wp_sprintf();
+} else {
+	echo wp_sprintf(
+		'<div %1$s><div class="wp-block-prc-block-collapsible__title"><div>%2$s</div><button class="wp-block-prc-block-collapsible__icon"><i aria-hidden="true" class="fa-light fa-circle-plus"></i><i aria-hidden="true" class="fa-light fa-circle-minus"></i></button></div><div class="wp-block-prc-block-collapsible__content">%3$s</div></div>',
+		$is_deprecated ? 'class="is-style-plus wp-block-prc-block-collapsible"' : get_block_wrapper_attributes(array('id' => sanitize_title($collapsible_title))),
+		$collapsible_title,
+		$content,
+	);
+}
---
+++ blocks/core-paragraph/class-apple-news.php
@@ -0,0 +1,37 @@
+<?php
+namespace Apple_Exporter\Components;
+use \Apple_Exporter\Exporter_Content;
+
+class Paragraph_Big_Number extends Component {
+	protected $can_be_anchor_target = true;
+	public $needs_layout_if_anchored = false;
+
+	public static function node_matches( $node ) {
+		return self::node_has_class( $node, 'is-style-has-big-number' ) ? $node : null;
+	}
+
+	public function register_specs(){
+		$this->register_spec(
+			'json',
+			__( 'JSON', 'apple-news' ),
+			array(
+				'role' => 'body',
+				'layout' => 'body-layout',
+				'textStyle' =>  '#style#',
+				'text' =>  '#text#',
+				'format' => 'html'
+			)
+		);
+	}
+
+	protected function build($html) {
+		preg_match('/<section.*data-count=\"(.*?)\"/i', $html, $match);
+		$count = intval($match[1]);
+		$innerHTML = preg_replace('/\<section\X*?>(\X*?)\<\/section>/i', '${1}', $html );
+		$innerHTML = preg_replace('/strong>/i', 'bignumstrong>', $innerHTML);
+		$this->register_json( 'json', array(
+			"#text#" =>  preg_replace('/<p>/i', '<p class="bignumber">', wpautop($count.$innerHTML)),
+			"#style#" => $count > 9 ? 'body-bignumber-2-digit' : 'body-bignumber'
+		) );
+	}
+}
