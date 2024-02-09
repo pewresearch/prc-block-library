bc0dd64970ce4f88238ea4f0ff60c0eae807636c
prcdevgitbot <webdev@pewresearch.org>
2024-02-09 18:08:10 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/8501ada7bf1e7ffc2c0960b1e31da91f1dd6850e from refs/heads/release/5.0

---
+++ blocks/interactive-island/interactive-island.php
@@ -0,0 +1,43 @@
+<?php
+namespace PRC\Platform\Blocks;
+
+/**
+ * Block Name:        Interactive Island
+ * Description:       Dynamically display and render changes server side to an island.
+ * Requires at least: 6.4
+ * Requires PHP:      8.1
+ * Author:            Pew Research Center
+ *
+ * @package           prc-block
+ */
+
+class interactive_island {
+	public static $block_library_version;
+	public static $active_theme;
+	public static $block_json;
+	public static $version;
+	
+	public static $dir = __DIR__;
+
+	public function __construct( $block_library_version, $active_theme ) {
+		self::$block_library_version = $block_library_version ? $block_library_version : null;
+		self::$active_theme = $active_theme ? $active_theme : null;
+		$block_json_file = PRC_BLOCK_LIBRARY_DIR . '/blocks/interactive-island/build/block.json';
+		self::$block_json = wp_json_file_decode( $block_json_file, array( 'associative' => true ) );
+		self::$block_json['file'] = wp_normalize_path( realpath( $block_json_file ) );
+		self::$version = self::$block_json['version'];
+	}
+
+	/**
+	* Registers the block using the metadata loaded from the `block.json` file.
+	* Behind the scenes, it registers also all assets so they can be enqueued
+	* through the block editor in the corresponding context.
+	* @hook init
+	*
+	* @see https://developer.wordpress.org/reference/functions/register_block_type/
+	*/
+	public function block_init() {
+		register_block_type( self::$dir . '/build' );
+	}
+
+}
---
+++ blocks/interactive-islands-controller/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'afa283348dca4820bd41');
