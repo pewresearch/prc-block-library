b36c529cb8bf34dec9e931b16d521b9fb2ac1ed9
prcdevgitbot <webdev@pewresearch.org>
2024-02-28 19:28:43 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/a61d5029501555d7721cfa957ccb201349c53b64 from refs/heads/release/5.0-bw-templates

--- blocks/logo/build/index.asset.php
+++ blocks/logo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('classnames', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '02c1dc33012174e67a88');
+<?php return array('dependencies' => array('classnames', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e99fee3644ed650973e0');
