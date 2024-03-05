360d8a347cfb76e2f46eddbf4195e6ea68cacf5f
prcdevgitbot <webdev@pewresearch.org>
2024-03-05 19:52:36 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/19aacfce60c7c07b63115af3f764c7a861717f6d from refs/heads/release/5.0-url-rewrites

--- blocks/core-navigation/src/index.js
+++ blocks/core-navigation/src/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress Dependencies
  */
+import { addFilter } from '@wordpress/hooks';
 import domReady from '@wordpress/dom-ready';
 import { registerBlockStyle, unregisterBlockStyle } from '@wordpress/blocks';
 
@@ -29,3 +30,22 @@ domReady(() => {
 		label: 'Divided Links',
 	});
 });
+
+addFilter(
+	'blocks.registerBlockType',
+	'prc-core-navigation-allowedblocks',
+	(settings, name) => {
+		if (name === 'core/navigation') {
+			return {
+				...settings,
+				allowedBlocks: [
+					...(settings.allowedBlocks ?? []),
+					'prc-user-accounts/login-logout',
+					'prc-block/logo',
+					'prc-block/navigation-mega-menu',
+				],
+			};
+		}
+		return settings;
+	}
+);
--- blocks/navigation-mega-menu/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => 'f2ebe9181c053737d1a3', 'type' => 'module');
