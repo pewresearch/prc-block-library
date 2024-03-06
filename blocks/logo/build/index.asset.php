4b021a48be449a1ba2e7eae3ccbde4a6e402f953
prcdevgitbot <webdev@pewresearch.org>
2024-03-06 18:40:53 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/d85966d4d9e8e511c2c25be09d0b48db097e7752 from refs/heads/release/5.0-url-rewrites

--- blocks/logo/build/index.asset.php
+++ blocks/logo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('classnames', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '70e46b6d760fb4aae6bb');
+<?php return array('dependencies' => array('classnames', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e14eaee38624a24afc15');
--- blocks/navigation-mega-menu/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => '3fb370222d77212a70f1', 'type' => 'module');
