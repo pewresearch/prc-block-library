6b429d907a57f0552328a208b3eef32f72b94766
prcdevgitbot <webdev@pewresearch.org>
2024-03-15 15:04:35 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/cdb97774ce18f77d71425f48dcd2e9240d22db88 from refs/heads/release/5.0

--- blocks/grid-controller/src/controls.jsx
+++ blocks/grid-controller/src/controls.jsx
@@ -8,7 +8,7 @@ import { MarkedRangeControl } from '@prc/components';
  * WordPress Dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { 
+import {
 	Notice,
 	RangeControl,
 	__experimentalToolsPanelItem as ToolsPanelItem,
@@ -25,6 +25,9 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import { createBlock } from '@wordpress/blocks';
 import { Fragment } from '@wordpress/element';
 
+const DESKTOP_MIN = 2;
+const TABLET_MIN = 2;
+const MOBILE_MIN = 2;
 const DESKTOP_MAX = 12;
 const TABLET_MAX = 8;
 const MOBILE_MAX = 4;
