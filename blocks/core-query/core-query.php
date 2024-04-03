1905690c99cf24754a4f508a037d929c617f33f2
prcdevgitbot <webdev@pewresearch.org>
2024-04-03 10:48:31 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/540189e12cded534643ac4995d4004b6311ca586 from refs/heads/release/5.0

--- blocks/core-query/core-query.php
+++ blocks/core-query/core-query.php
@@ -177,7 +177,7 @@ class Core_Query {
 		if ( ( $query->is_home() || $query->is_main_query() || $query->is_tax() || $query->is_archive() || $query->is_category() || $query->post_type_archive() ) && ! $query->is_post_type_archive( [
 			'short-read',
 		] ) ) {
-			$query->set('post_type', self::$post_type_query_arg);
+			// $query->set('post_type', self::$post_type_query_arg);
 		}
 	}
 
