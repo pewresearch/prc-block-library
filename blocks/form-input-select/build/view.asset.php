6794cb8ae51677be6ed5502cbf3b9c596faf6155
prcdevgitbot <webdev@pewresearch.org>
2024-04-17 21:32:07 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/a3624b03b1aecf7dcf65d8fe558f26ad9308adc3 from refs/heads/release/1.0-chris

--- blocks/form-input-select/build/view.asset.php
+++ blocks/form-input-select/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => 'dca50ec8d233d7110ace', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => 'c26f9af222636add2783', 'type' => 'module');
